0adcc4006845e895d60417a82bc8c014108eb123
stevenjokes <a867907127@gmail.com>
2020-07-01 02:54:44 +0800

    c++ 适配成功

---
+++ .history/string_processing/decimal_to_binary_20200701015119.cpp
@@ -0,0 +1,28 @@
+
+
+/*
+ * @version:
+ * @Author:  StevenJokes https://github.com/StevenJokes
+ * @Date: 2020-06-30 23:12:40
+ * @LastEditors:  StevenJokes https://github.com/StevenJokes
+ * @LastEditTime: 2020-07-01 01:51:18
+ * @Description:
+ * @TODO::
+ * @Reference:
+ */
+//
+// Created by 安炳旭 on 2020-03-02.
+//
+
+//递归实现十进制数组转二进制
+
+#include <stdio.h>
+void fun(int n){
+   if(n/2)
+       fun(n/2);
+   printf("%d",n%2);
+}
+
+int main(){
+   fun(128);
+}
